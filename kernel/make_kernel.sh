120947ca0f5de60d5ffe980732c3860ec90fe6fd
John Clark <inindev@gmail.com>
2023-07-05 18:45:01 -0400

    update to linux 6.4.2
    Signed-off-by: John Clark <inindev@gmail.com>

--- kernel/make_kernel.sh
+++ kernel/make_kernel.sh
@@ -16,8 +16,8 @@ config_fixups() {
 }
 
 main() {
-    local linux='https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.1.tar.xz'
-    local lxsha='0d9daa9f1c176fb13b9447f6e3d80e82b49043f0d344c247bbf09b4e625beef3'
+    local linux='https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.2.tar.xz'
+    local lxsha='a326ab224176c5b17c73c9ccad85f32e49b6e4e764861d57595727b7ef10062c'
 
     local lf="$(basename "$linux")"
     local lv="$(echo "$lf" | sed -nE 's/linux-(.*)\.tar\..z/\1/p')"
